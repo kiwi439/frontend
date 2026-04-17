82194376bea06feea31a998417f37b21b8bf3642
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2026-04-17 16:12:21 +0200

    Add cancel payment page

--- src/pages/AppRouter.tsx
+++ src/pages/AppRouter.tsx
@@ -14,6 +14,7 @@ import Newsletter from 'pages/userPanel/Newsletter/Newsletter';
 import RemoveAccount from 'pages/userPanel/RemoveAccount';
 import Avatars from 'pages/userPanel/Avatars/Avatars.jsx';
 import NotFound from 'pages/NotFound';
+import PaymentCanceled from 'pages/PaymentCanceled';
 
 const AppRouter = () => (
   <div className="main">
@@ -25,6 +26,7 @@ const AppRouter = () => (
       <Route path="/opinions" element={<Opinions />} />
       <Route path="/order" element={<Order />} />
       <Route path="/thank-you-page" element={<ThankYouPage />} />
+      <Route path="/payment-canceled" element={<PaymentCanceled />} />
       <Route path="/user-panel" element={<UserPanel />}>
         <Route path="" element={<MyDetails />} />
         <Route path="data" element={<MyDetails />} />
---
+++ src/pages/PaymentCanceled.tsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ShadowedContainer from 'components/containers/ShadowedContainer';
+import SubmitButton from 'components/SubmitButton';
+
+const PaymentCanceled = () => {
+  const blockName = 'payment-canceled';
+  const navigate = useNavigate();
+
+  return (
+    <ShadowedContainer classNames={blockName}>
+      <Fragment>
+        <h1 className={`${blockName}__header`}>Płatność anulowana</h1>
+        <p className={`${blockName}__description`}>
+          Nie udalo sie sfinalizowac platnosci. Możesz spróbować ponownie.
+        </p>
+        <SubmitButton
+          value="Wróć do zamówienia"
+          classNames={`${blockName}__button`}
+          onMouseDown={() => navigate('/')}
+        />
+      </Fragment>
+    </ShadowedContainer>
+  );
+};
+
+export default PaymentCanceled;
