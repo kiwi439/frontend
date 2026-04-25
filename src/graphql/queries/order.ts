c2d2137b726b5751a2e79d4a37a5a02498877b84
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2026-04-25 10:33:08 +0200

    Add Thank you page logic

--- src/graphql/queries/order.ts
+++ src/graphql/queries/order.ts
@@ -1,5 +1,19 @@
 import { gql } from '@apollo/client';
 
+export const GET_ORDER = gql`
+  query Order($id: ID!) {
+    order(id: $id) {
+      paid
+      latestPayment {
+        id
+        status
+        amountCents
+        provider
+      }
+    }
+  }
+`;
+
 export const GET_ORDERS = gql`
   query Orders ($first: Int, $after: String, $userId: ID) {
     orders(first: $first, after: $after, userId: $userId) {
