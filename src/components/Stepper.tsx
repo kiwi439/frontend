ebf07008392ecc38d7eab1e782345fdc9d493e3b
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2026-04-26 22:19:37 +0200

    fix style

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -17,7 +17,12 @@ const Stepper = ({ activeStep, handleStepOnClick, labels }: StepperType) => {
         labels.map((label, index) => (
           <Step key={uuidv4()}>
             <StepLabel
-              className={`${blockName}__label ${index < activeStep ? `${blockName}__label--clickable` : ''}`}
+              className={
+                `${blockName}__label
+                 ${index < activeStep ? `${blockName}__label--completed` : ''}
+                 ${index === activeStep ? `${blockName}__label--active` : ''}
+                 ${index > activeStep ? `${blockName}__label--upcoming` : ''}`
+              }
               onClick={() => handleStepOnClick(index)}
             >
               {label}
